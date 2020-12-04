742d041c13dc52ea5b006c23986439cc66baeef4
dierp <di-rp@hotmail.com>
2020-12-04 09:52:56 -0300

    feat: add express and sucrase

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,4 @@
-// const knexProd = require('knex')({
-//   client: 'mysql',
-//   connection: {
-//     host: 'localhost',
-//     port: 3306,
-//     database: 'jsts',
-//     user: 'root',
-//     password: 'root',
-//     // host: process.env.APP_DB_HOST,
-//     // port: process.env.APP_DB_PORT,
-//     // database: process.env.APP_DB_NAME,
-//     // user: process.env.APP_DB_USER,
-//     // password: process.env.APP_DB_PASSWORD,
-//     connectTimeout: 90000
-//   },
-//   pool: {
-//     min: 1,
-//     max: 20
-//   },
-//   migrations: {
-//     tableName: 'knex_migrations',
-//     directory: './src/infra/db/knex/migrations'
-//   }
-// })
-// const knexTest = require('knex')({
-//   client: 'mysql',
-//   connection: {
-//     host: 'localhost',
-//     port: 3306,
-//     database: 'jsts-test',
-//     user: 'root',
-//     password: 'root',
-//     connectTimeout: 90000
-//   },
-//   pool: {
-//     min: 1,
-//     max: 20
-//   },
-//   migrations: {
-//     tableName: 'knex_migrations',
-//     directory: './src/infra/db/knex/migrations'
-//   }
-// })
-// module.exports = { knexProd, knexTest }
+
 require('dotenv').config()
 module.exports = {
   test: {
--- src/infra/db/knex.js
+++ src/infra/db/knex.js
@@ -1,4 +1,4 @@
-var environment = process.env.NODE_ENV || 'development'
+var environment = process.env.NODE_ENV || 'test'
 var config = require('../../../knexfile')[environment]
 
 module.exports = require('knex')(config)
