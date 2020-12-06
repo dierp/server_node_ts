721c276166c2f9841146a8f18f2712627bd18f29
dierp <di-rp@hotmail.com>
2020-12-06 14:04:50 -0300

    test: add knex connection before integration

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,17 @@
 import app from '../config/app'
 import request from 'supertest'
+import knex from '../../infra/db/knex'
 
 describe('Signup Routes', () => {
+  beforeAll(async () => {
+    return await knex.migrate.latest()
+    // you can here also seed your tables, if you have any seeding files
+  })
+  afterAll(async () => {
+    return await knex.migrate
+      .rollback()
+      .then(async () => await knex.destroy())
+  })
   test('Should return an user on success', async () => {
     await request(app)
       .post('/api/signup')
